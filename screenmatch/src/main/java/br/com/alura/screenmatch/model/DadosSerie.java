94844824b95bfc2a9a0ecaf14a2c8068e70b295c
suhellynaraujo <github.interface@gmail.com>
2024-07-14 19:43:29 -0300

    response api

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 // @JsonAlias usa- se para dar apelidos aos argumentos, para serem reconhecidos como são passados nos parametros da resposta da api
+//@JsonIgnoreProperties ignora todos os demais argumentos não listados
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating")String avaliacao) {
---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -0,0 +1,8 @@
+package br.com.alura.screenmatch.service;
+
+public interface IConverteDados {
+
+   <T> T obterDados(String json, Class<T> classe);
+
+
+}
