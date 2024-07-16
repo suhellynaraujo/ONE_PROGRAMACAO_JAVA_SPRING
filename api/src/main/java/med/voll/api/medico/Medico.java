abfa83f22fcbfe3f003e581f44302d7f77037f11
suhellynaraujo <github.interface@gmail.com>
2024-07-16 18:31:19 -0300

    mapeando entidade

---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,33 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+//identidade JPA
+@Table(name = "medicos")
+@Entity(name = "Medico")
+// anotação lo lombpok para não precisar gerar os getter e setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    //gerar o id automaticamente
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    // para anaotar que é um enum
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    //anotar para ficar como uma classe separada, mas considera os mesmos campos da tabela de medicos
+    @Embedded
+    private Endereco endereco;
+}
