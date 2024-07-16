67e03606b53c6f3e9ac2b8392ca4d1697dce68fc
suhellynaraujo <github.interface@gmail.com>
2024-07-16 17:46:50 -0300

    record DadosEndereco

---
+++ api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,9 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(String lopgradouro,
+                            String bairro,
+                            String cep,
+                            String cidade,
+                            String uf,String complemento,
+                            String numero) {
+}
